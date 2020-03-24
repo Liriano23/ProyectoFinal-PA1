c468a02fc1d373ece56ded13780f825105cfdf1c
Enmanuel Gonzalez Liriano <enmanuel_gonzalez1@ucne.edu.do>
2020-03-23 22:35:24 -0400

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ProyectoFinal_PA1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show($"Ocurrio una excepcion no controlada or {e.Exception.Message}");
+            e.Handled = true;
+        }
+    }
+}
