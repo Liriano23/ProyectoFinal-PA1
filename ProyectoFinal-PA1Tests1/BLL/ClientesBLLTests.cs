c246cbd974a6658aa409b128afc2c6ac4b4274c2
Juan De Dios Fernandez Camilo <juan_fernandez3@ucne.edu.do>
2020-03-30 02:37:36 -0400

    se Agrego el test a todas las BLL

---
+++ ProyectoFinal-PA1Tests1/BLL/ClientesBLLTests.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProyectoFinal_PA1.BLL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ProyectoFinal_PA1.Entidades;
+
+namespace ProyectoFinal_PA1.BLL.Tests
+{
+    [TestClass()]
+    public class ClientesBLLTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            bool paso;
+            Clientes clientes = new Clientes();
+
+            clientes.ClienteId = 0;
+            clientes.Nombres = "Enmanuel";
+            clientes.Apellidos = "Gonzalez liriano";
+            clientes.Cedula = "888-888-888";
+            clientes.Sexo = "masculino";
+            clientes.Direccion = "Deep Park";
+            clientes.Telefono = "888-888-888";
+            clientes.Celular = "888-888-888";
+            clientes.Email = "lol";
+            clientes.FechaNacimiento = DateTime.Now;
+            clientes.FechaIngreso = DateTime.Now;
+            clientes.UsuariosId = 0;
+
+            paso = ClientesBLL.Guardar(clientes);
+            Assert.AreEqual(paso, true);
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            bool paso;
+            Clientes clientes = new Clientes();
+
+            clientes.ClienteId = 0;
+            clientes.Nombres = "juan de dios";
+            clientes.Apellidos = "Fernandez camilo";
+            clientes.Cedula = "111-888-888";
+            clientes.Sexo = "masculino";
+            clientes.Direccion = "Park";
+            clientes.Telefono = "111-888-888";
+            clientes.Celular = "111-888-888";
+            clientes.Email = "hola";
+            clientes.FechaNacimiento = DateTime.Now;
+            clientes.FechaIngreso = DateTime.Now;
+            clientes.UsuariosId = 0;
+
+            paso = ClientesBLL.Guardar(clientes);
+            Assert.AreEqual(paso, true);
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            bool paso;
+            Clientes clientes = new Clientes();
+
+            clientes.ClienteId = 7;
+            paso = ClientesBLL.Eliminar(clientes.ClienteId);
+            Assert.AreEqual(paso, true);
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ ProyectoFinal-PA1Tests1/BLL/SuplidoresBLLTests.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProyectoFinal_PA1.BLL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ProyectoFinal_PA1.Entidades;
+
+namespace ProyectoFinal_PA1.BLL.Tests
+{
+    [TestClass()]
+    public class SuplidoresBLLTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            bool paso;
+            Suplidores suplidores = new Suplidores();
+
+            suplidores.SuplidorId = 0;
+            suplidores.NombreSuplidor = "Enmanuel";
+            suplidores.Apellidos = "Gonzalez liriano";
+            suplidores.NombreCompania = "Michelin";
+            suplidores.Direccion = "SFM";
+            suplidores.Telefono = "111-888-888";
+            suplidores.Celular = "111-888-888";
+            suplidores.Email = "hola";
+            suplidores.UsuariosId = 1;
+
+            paso = SuplidoresBLL.Guardar(suplidores);
+            Assert.AreEqual(paso, true);
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            bool paso;
+            Suplidores suplidores = new Suplidores();
+
+            suplidores.SuplidorId = 0;
+            suplidores.NombreSuplidor = "juan";
+            suplidores.Apellidos = "ferandez ";
+            suplidores.NombreCompania = "Michelin";
+            suplidores.Direccion = "Santigo";
+            suplidores.Telefono = "000-888-888";
+            suplidores.Celular = "000-888-888";
+            suplidores.Email = "hey";
+            suplidores.UsuariosId = 1;
+
+            paso = SuplidoresBLL.Guardar(suplidores);
+            Assert.AreEqual(paso, true);
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            bool paso;
+            Suplidores suplidores = new Suplidores();
+
+            suplidores.SuplidorId = 7;
+            paso = SuplidoresBLL.Eliminar(suplidores.SuplidorId);
+            Assert.AreEqual(paso, true);
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
